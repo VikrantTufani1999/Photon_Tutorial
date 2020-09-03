5a58cbc59964d642e578c5e4bf383d533e66bacf
Vikrant Tufani <8052007294vicky@gmail.com>
2020-09-03 15:08:02 +0530

    Updated on 03.09.2020

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(photonView.IsMine)
+        {
+            transform.GetComponent<MovementController>().enabled = true;
+            FPSCamera.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            FPSCamera.GetComponent<Camera>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
